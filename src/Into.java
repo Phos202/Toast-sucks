baae2af4e5dc112c2d74cddfdbe093b39d4e4706
Phos202 <999.com.com.com@gmail.com>
2021-11-18 19:54:38 -0500

    Moved stuff from replit to dev code

--- src/Into.java
+++ src/Into.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
 public class Into {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Look");
+      Scanner scan = new Scanner(System.in);
+
+
+
+
+      int lesson = 0;//LESSON PICKER
+      int mpart = 0;//section parts for Math
+    int Spart = 0; //sections for strings
+
+    
+    
+   
+    System.out.println("Welcome to the start of your Java programing.\n\n Please Selcet a number to being a lesson");
+    
+    
+    //Strings lesson
+    System.out.println("\nStrings = 1");
+    
+    //Everything Math basics
+    System.out.println("\nMath basics = 2");
+    
+    
+    lesson = scan.nextInt();
+    //make it so when they pick a number they pick a lesson and the lesson picked loops between parts withing the lesson
+    // if(lesson == 1)
+    // { 
+    // Sintro;
+    // }
+
     }
 }
---
+++ src/oneStrings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class oneStrings {
+    
+
+    public class oneString{ // intro to strings
+        Scanner scan = new Scanner(System.in);
+            
+        public String Sintro()
+        {
+          return "Welcome to Lesson Strings in this lesson you will get to learn the basics of using strings";
+        }
+
+
+}
+
+}
