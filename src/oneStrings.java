46fd259102cc3570835436585455d568d7d3f4c9
Alexandar E E <56139315+Phos202@users.noreply.github.com>
2021-11-19 13:14:28 +0000

    Comments on the code for ease of programing

--- src/oneStrings.java
+++ src/oneStrings.java
@@ -6,8 +6,9 @@ public class oneStrings {
         public String Sintro()
         {    // intro to strings
           return "Welcome to Lesson Strings in this lesson you will get to learn the basics of using strings";
-        }
-public String Printing()
+        }//Show how to print 
+
+public String hPrinting()// how to print and type them
 {
   
 
