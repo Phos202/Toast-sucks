a774d1f3fb250cb96aa633342a2699875bb0752e
Phos202 <999.com.com.com@gmail.com>
2021-11-18 19:32:28 -0500

    what the ]

--- src/Into.java
+++ src/Into.java
@@ -1,6 +1,6 @@
 public class Into {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Look at the world");
+        System.out.println("Look");
     }
 }
