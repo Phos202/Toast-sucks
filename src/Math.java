e9f04bb2646a0612eee0959a601b84b73f5f0c6f
Phos202 <56139315+Phos202@users.noreply.github.com>
2021-11-22 12:15:25 -0500

    Update Math.java
    Work on the inro more for math basics so the user understand that ints can hold a value of a double

--- src/Math.java
+++ src/Math.java
@@ -11,7 +11,7 @@ double d;
  
     public String introint()//intro lesson to ints
     {
-return null;
+return "Welcome to the basics of java math\n\nLets get right into it starting wiht INTS!!!!\n\nInts: ints are a type of primitive value that hold a value of != decimal,\n\tlike 100 90000 3000 NOT 0.9,3.9 you get the gist";
     }
 
 public void ints()//teach ints
