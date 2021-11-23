97e9b965b2f490bfb3093a25d259acac0bc79c4b
Phos202 <56139315+Phos202@users.noreply.github.com>
2021-11-23 08:48:36 -0500

    Update AOperator.java
    Into to for loops added

--- src/AOperator.java
+++ src/AOperator.java
@@ -14,8 +14,9 @@ oneStrings introS = new oneStrings(huh);
 
 
 
-//make intro for for loop lesson
-    
+//make intro for for For loop lesson
+    System.out.println("\nWelcome to Lesson For. Here you will learn how to use for loops and maybe some intesting ways to use.\n\tYou will get to try this out\n\n\t**Note to the user, making this work was more than a hassle that its should've been** ");
+        
     System.out.println("Select your ints num");//user work
 int Fori = scan.nextInt();
 
