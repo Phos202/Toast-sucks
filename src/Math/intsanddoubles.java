1b2e0d6c7549528dda6e30a110af0d8f9c0c7c17
Phos202 <56139315+Phos202@users.noreply.github.com>
2022-01-27 08:03:05 -0500

    Update intsanddoubles.java

--- src/Math/intsanddoubles.java
+++ src/Math/intsanddoubles.java
@@ -1,11 +1,7 @@
 package Math;
 
 public class intsanddoubles {
-    private int i1;
-        private int i2;
-        
-    private double d1;
-        private double d2;
+   String notes = "";
 
         public intsanddoubles()
         {
