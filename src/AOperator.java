4dbd1d0c361dbb6c730cea47bec363bab454a0e2
Phos202 <56139315+Phos202@users.noreply.github.com>
2022-01-27 07:53:16 -0500

    Runner
    Added stuff so that the user can call the math class 
    
    *TO DO*
    
    Make it so in runner the user does the code then teaches them the lesson in class

--- src/AOperator.java
+++ src/AOperator.java
@@ -4,9 +4,13 @@ public class AOperator {
       Scanner scan = new Scanner(System.in);
 System.out.println("Welcome to your easy java learning course,\nHere You Will begin with learning the basics of math !WHEN YOU WANT TO MOVE ON TO THE NEXT LESSON YOU WILL BE PROMTED WHEN!\nIf you want to practice go to Replit.com");
 
-
-
-
+    System.out.println("To pick lesson intsanddoubles please pick 1 Lesson;l Strings pick 2");
+int user10 = scan.nextInt();
+            
+        if(user10 = 1)
+        {
+         intsanddoubles();
+        }
 
 
             
