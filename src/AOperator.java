7a1b53a8abaa787fbf58f308ea318d7e87c703fc
Phos202 <999.com.com.com@gmail.com>
2022-01-26 23:39:41 -0500

    why more

--- src/AOperator.java
+++ src/AOperator.java
@@ -2,15 +2,11 @@ import java.util.Scanner;
 public class AOperator {
     public static void main(String[] args) throws Exception {
       Scanner scan = new Scanner(System.in);
+System.out.println("Welcome to your easy java learning course,\nHere You Will begin with learning the basics of math !WHEN YOU WANT TO MOVE ON TO THE NEXT LESSON YOU WILL BE PROMTED WHEN!\nIf you want to practice go to Replit.com");
 
 
-String huh = "";
 
 
-
-System.out.println("Welcome to Lesson Strings in this lesson you will get to learn the basics of using strings");
-        System.out.println("\nThe basic examples of Strings in java are the print types, one is System.out.print(); and the other is \n System.out.println();");
-            System.out.println("\n In order to print words in the print either have \"\" or have an String *ANYNAME* = \"*Can equal any string and any words put in the quotes*\"");
 
 
             
@@ -19,15 +15,12 @@ System.out.println("Welcome to Lesson Strings in this lesson you will get to lea
       
 
 
-
+//Lesson ints and doubles
 
 
 
 //make intro for for For loop lesson
-    System.out.println("\nWelcome to Lesson For. Here you will learn how to use for loops and maybe some intesting ways to use.\n\tYou will get to try this out\n\n\t**Note to the user, making this work was more than a hassle that its should've been** ");
-        
-    System.out.println("\nSelect the amount of numbers wanted for loop");//user work
-int arrayamnt = scan.nextInt();
+   
 
 
 
