3d6a9c59be86cde2fd4e7ec806238f20ed8dfda6
Phos202 <999.com.com.com@gmail.com>
2022-01-26 23:38:08 -0500

    Made a basic math class for teaching ints in d

---
+++ src/Math/intsanddoubles.java
@@ -0,0 +1,24 @@
+package Math;
+
+public class intsanddoubles {
+    private int i1;
+        private int i2;
+        
+    private double d1;
+        private double d2;
+
+        public intsanddoubles()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+}
