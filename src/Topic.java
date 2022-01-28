5e97d8fb7fc865fd1492b2869e90104a87445836
Phos15 <56139315+Phos202@users.noreply.github.com>
2022-01-28 08:51:10 -0500

    Update Topic.java

--- src/Topic.java
+++ src/Topic.java
@@ -4,23 +4,29 @@ import java.util.Scanner;
 
 public class Topic
 {
+  private String name;
+private String notes;
       
-private int selc;
-Scanner scan = new Scanner(System.in);
-  
 
 
-    public void tNotes(String[] tname, String[] notes)
-    { 
-      System.out.println("\nSelect a lesson/note(1-5)");
-      selc = scan.nextInt(); 
+  public Topic(String n, String no)
+  {
+    this.name = n;
+    this.notes = no;
+  }
 
-    }
+
+    // public void tNotes(String[] tname, String[] notes)
+    // { 
+     
+//  System.out.println("\nSelect a lesson/note(0-4)");
+//       selc = scan.nextInt(); 
+    // }
  
-      //     public String toString()
-      //     {
-      //     return "This is " + tname[selc] +notes[selc];
-      //     }
+  public String toString()
+  {
+  return "This is " + name +"\n"+notes;
+  }
   
   
   
